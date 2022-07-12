b8c3aa6366aaac1bcdd41143bfc79654794c8faa
shivanandvp-official <shivanandvp.oss@gmail.com>
2022-07-12 16:36:54 +0000

    Update from https://github.com/shivanandvp-official/personal-website/commit/1ffb1ac5ab4a00469cf78d823540e5387fa9b3bd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"index.html": "4b9e65de9ad5942a64d4f0254f617ac0",
-"/": "4b9e65de9ad5942a64d4f0254f617ac0",
+"index.html": "4e5114cbeb65bed5882cea7d231d82e5",
+"/": "4e5114cbeb65bed5882cea7d231d82e5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "main.dart.js": "79d9b739c82ddff3d8bd43ceb4bd59f6"
 };
